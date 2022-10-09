307c8aba34c9e00637f663cb32ae342ba9153163
Jonathan Hopper <90158461+jonhopper-dataengineers@users.noreply.github.com>
2022-10-09 17:42:51 +1300

    changes to add support for snapshots data masking (#45)
    * Snapshots (#1)
    
    * added in ability to process snapshots
    
    * added unapply resource type for snapshots
    
    * added in snapshot test

--- macros/snow-mask/apply-policy/unapply_masking_policy.sql
+++ macros/snow-mask/apply-policy/unapply_masking_policy.sql
@@ -4,7 +4,7 @@
 
         {% if resource_type == "sources" %}
             {{ dbt_snow_mask.apply_masking_policy_list_for_sources(meta_key,operation_type) }}
-        {% elif resource_type == "models" %}
+        {% elif resource_type|lower in ["models", "snapshots"] %}
             {{ dbt_snow_mask.apply_masking_policy_list_for_models(meta_key,operation_type) }}
         {% endif %}
 
