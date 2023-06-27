62dcebfced0a5a5a1b02c82b3f66f5930d6dd07f
melissa-ellevation <melissa.ragonese@ellevationeducation.com>
2023-06-27 14:03:07 -0400

    add for loop thru nodes to apply_masking_policy_list_for_models.sql

--- macros/snow-mask/apply-policy/apply_masking_policy_list_for_models.sql
+++ macros/snow-mask/apply-policy/apply_masking_policy_list_for_models.sql
@@ -3,75 +3,79 @@
 {% if execute %}
 
     {% if operation_type == "apply" %}
+
+        {% for model in graph.nodes.values() %}
     
-        {% set model_id = model.unique_id | string %}
-        {% set alias    = model.alias %}    
-        {% set database = model.database %}
-        {% set schema   = model.schema %}
-        {% set model_resource_type = model.resource_type | string %}
+            {% set model_id = model.unique_id | string %}
+            {% set alias    = model.alias %}
+            {% set database = model.database %}
+            {% set schema   = model.schema %}
+            {% set model_resource_type = model.resource_type | string %}
 
-        {% if model_resource_type|lower in ["model", "snapshot"] %}
+            {% if model_resource_type|lower in ["model", "snapshot"] %}
 
-            {# This dictionary stores a mapping between materializations in dbt and the objects they will generate in Snowflake  #}
-            {% set materialization_map = {"table": "table", "view": "view", "incremental": "table", "snapshot": "table"} %}
+                {# This dictionary stores a mapping between materializations in dbt and the objects they will generate in Snowflake  #}
+                {% set materialization_map = {"table": "table", "view": "view", "incremental": "table", "snapshot": "table"} %}
 
-            {# Append custom materializations to the list of standard materializations  #}
-            {% do materialization_map.update(fromjson(var('custom_materializations_map', '{}'))) %}
+                {# Append custom materializations to the list of standard materializations  #}
+                {% do materialization_map.update(fromjson(var('custom_materializations_map', '{}'))) %}
 
-            {% set materialization = materialization_map[model.config.get("materialized")] %}
-            {% set meta_columns = dbt_snow_mask.get_meta_objects(model_id,meta_key) %}
+                {% set materialization = materialization_map[model.config.get("materialized")] %}
+                {% set meta_columns = dbt_snow_mask.get_meta_objects(model_id,meta_key) %}
 
-            {% set masking_policy_db = model.database %}
-            {% set masking_policy_schema = model.schema %}
+                {% set masking_policy_db = model.database %}
+                {% set masking_policy_schema = model.schema %}
             
-            {# Override the database and schema name when use_common_masking_policy_db flag is set #}
-            {%- if (var('use_common_masking_policy_db', 'False')|upper in ['TRUE','YES']) -%}
-                {% if (var('common_masking_policy_db') and var('common_masking_policy_schema')) %}
-                    {% set masking_policy_db = var('common_masking_policy_db') | string  %}
-                    {% set masking_policy_schema = var('common_masking_policy_schema') | string  %}
+                {# Override the database and schema name when use_common_masking_policy_db flag is set #}
+                {%- if (var('use_common_masking_policy_db', 'False')|upper in ['TRUE','YES']) -%}
+                    {% if (var('common_masking_policy_db') and var('common_masking_policy_schema')) %}
+                        {% set masking_policy_db = var('common_masking_policy_db') | string  %}
+                        {% set masking_policy_schema = var('common_masking_policy_schema') | string  %}
+                    {% endif %}
                 {% endif %}
-            {% endif %}
 
-            {# Override the schema name (in the masking_policy_db) when use_common_masking_policy_schema_only flag is set #}
-            {%- if (var('use_common_masking_policy_schema_only', 'False')|upper in ['TRUE','YES']) and (var('use_common_masking_policy_db', 'False')|upper in ['FALSE','NO']) -%}
-                {% if var('common_masking_policy_schema') %}
-                    {% set masking_policy_schema = var('common_masking_policy_schema') | string  %}
+                {# Override the schema name (in the masking_policy_db) when use_common_masking_policy_schema_only flag is set #}
+                {%- if (var('use_common_masking_policy_schema_only', 'False')|upper in ['TRUE','YES']) and (var('use_common_masking_policy_db', 'False')|upper in ['FALSE','NO']) -%}
+                    {% if var('common_masking_policy_schema') %}
+                        {% set masking_policy_schema = var('common_masking_policy_schema') | string  %}
+                    {% endif %}
                 {% endif %}
-            {% endif %}
 
-            {% set masking_policy_list_sql %}     
-                show masking policies in {{masking_policy_db}}.{{masking_policy_schema}};
-                select $3||'.'||$4||'.'||$2 as masking_policy from table(result_scan(last_query_id()));
-            {% endset %}
+                {% set masking_policy_list_sql %}
+                    show masking policies in {{masking_policy_db}}.{{masking_policy_schema}};
+                    select $3||'.'||$4||'.'||$2 as masking_policy from table(result_scan(last_query_id()));
+                {% endset %}
 
-            {# If there are some masking policies to be applied in this model, we should show the masking policies in the schema #}
-            {% if meta_columns | length > 0 %}
-                {% set masking_policy_list = dbt_utils.get_query_results_as_dict(masking_policy_list_sql) %}
-            {% endif %}
+                {# If there are some masking policies to be applied in this model, we should show the masking policies in the schema #}
+                {% if meta_columns | length > 0 %}
+                    {% set masking_policy_list = dbt_utils.get_query_results_as_dict(masking_policy_list_sql) %}
+                {% endif %}
 
-            {%- for meta_tuple in meta_columns if meta_columns | length > 0 %}
-                {% set column               = meta_tuple[0] %}
-                {% set masking_policy_name  = meta_tuple[1] %}
-                {% set conditional_columns  = meta_tuple[2] %}
+                {%- for meta_tuple in meta_columns if meta_columns | length > 0 %}
+                    {% set column               = meta_tuple[0] %}
+                    {% set masking_policy_name  = meta_tuple[1] %}
+                    {% set conditional_columns  = meta_tuple[2] %}
                 
-                {% if masking_policy_name is not none %}
+                    {% if masking_policy_name is not none %}
 
-                    {% for masking_policy_in_db in masking_policy_list['MASKING_POLICY'] %}
-                        {% if masking_policy_db|upper ~ '.' ~ masking_policy_schema|upper ~ '.' ~ masking_policy_name|upper == masking_policy_in_db %}
-                            {{ log(modules.datetime.datetime.now().strftime("%H:%M:%S") ~ " | " ~ operation_type ~ "ing masking policy to model  : " ~ masking_policy_db|upper ~ '.' ~ masking_policy_schema|upper ~ '.' ~ masking_policy_name|upper ~ " on " ~ database ~ '.' ~ schema ~ '.' ~ alias ~ '.' ~ column ~ ' [force = ' ~ var('use_force_applying_masking_policy','False') ~ ']', info=True) }}
-                            {% set query %}
-                            alter {{materialization}} {{database}}.{{schema}}.{{alias}}
-                            modify column {{column}}
-                            set masking policy {{masking_policy_db}}.{{masking_policy_schema}}.{{masking_policy_name}} {% if conditional_columns | length > 0 %}using ({{column}}, {{conditional_columns|join(', ')}}){% endif %} {% if var('use_force_applying_masking_policy','False')|upper in ['TRUE','YES'] %} force {% endif %};
-                            {% endset %}
-                            {% do run_query(query) %}
-                        {% endif %}
-                    {% endfor %}
+                        {% for masking_policy_in_db in masking_policy_list['MASKING_POLICY'] %}
+                            {% if masking_policy_db|upper ~ '.' ~ masking_policy_schema|upper ~ '.' ~ masking_policy_name|upper == masking_policy_in_db %}
+                                {{ log(modules.datetime.datetime.now().strftime("%H:%M:%S") ~ " | " ~ operation_type ~ "ing masking policy to model  : " ~ masking_policy_db|upper ~ '.' ~ masking_policy_schema|upper ~ '.' ~ masking_policy_name|upper ~ " on " ~ database ~ '.' ~ schema ~ '.' ~ alias ~ '.' ~ column ~ ' [force = ' ~ var('use_force_applying_masking_policy','False') ~ ']', info=True) }}
+                                {% set query %}
+                                alter {{materialization}} {{database}}.{{schema}}.{{alias}}
+                                modify column {{column}}
+                                set masking policy {{masking_policy_db}}.{{masking_policy_schema}}.{{masking_policy_name}} {% if conditional_columns | length > 0 %}using ({{column}}, {{conditional_columns|join(', ')}}){% endif %} {% if var('use_force_applying_masking_policy','False')|upper in ['TRUE','YES'] %} force {% endif %};
+                                {% endset %}
+                                {% do run_query(query) %}
+                            {% endif %}
+                        {% endfor %}
 
-                {% endif %}
-            {% endfor %}
+                    {% endif %}
+                {% endfor %}
 
-        {% endif %}
+            {% endif %}
+
+        {% endfor %}
     
     {% elif operation_type == "unapply" %}
 
