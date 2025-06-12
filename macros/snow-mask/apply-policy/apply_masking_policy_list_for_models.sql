1736519466ac077837b89bfcbdc4f6771897b782
entechlog <38563714+entechlog@users.noreply.github.com>
2025-06-12 10:34:12 -0700

    Update macros/snow-mask/apply-policy/apply_masking_policy_list_for_models.sql
    Co-authored-by: Jose Escudero <9829156+josescuderoh@users.noreply.github.com>

--- macros/snow-mask/apply-policy/apply_masking_policy_list_for_models.sql
+++ macros/snow-mask/apply-policy/apply_masking_policy_list_for_models.sql
@@ -62,7 +62,7 @@
                             {{ log(modules.datetime.datetime.now().strftime("%H:%M:%S") ~ " | " ~ operation_type ~ "ing masking policy to model  : " ~ masking_policy_db|upper ~ '.' ~ masking_policy_schema|upper ~ '.' ~ masking_policy_name|upper ~ " on " ~ database ~ '.' ~ schema ~ '.' ~ alias ~ '.' ~ column ~ ' [force = ' ~ var('use_force_applying_masking_policy','False') ~ ']', info=True) }}
                             {% set query %}
                             alter {{materialization}} {{database}}.{{schema}}.{{alias}}
-                            modify column {{column}}
+                            modify column {{quoted_column}}
                             set masking policy {{masking_policy_db}}.{{masking_policy_schema}}.{{masking_policy_name}} {% if conditional_columns | length > 0 %}using ({{quoted_column}}, {{conditional_columns|join(', ')}}){% endif %} {% if var('use_force_applying_masking_policy','False')|upper in ['TRUE','YES'] %} force {% endif %};
                             {% endset %}
                             {% do run_query(query) %}
